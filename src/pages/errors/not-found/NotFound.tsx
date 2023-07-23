8db95d488d34612c18bdb4cf996120114ecbd7cf
Georgiy <sadkovdanil64@gmail.com>
2023-07-24 02:41:45 +0300

    cretet error page

--- src/pages/errors/not-found/NotFound.tsx
+++ src/pages/errors/not-found/NotFound.tsx
@@ -4,10 +4,25 @@ import {useLocation, useNavigate, useNavigation} from "react-router-dom";
 
 export const NotFound: FC = () => {
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    }
+
+    const goHome = () => {
+        navigate('/', {replace: true})
+    }
 
   return (
-    <div className={s.error_page}>
-        <p>404 | Page not found on path <span className={s.path_name}>{location.pathname}</span></p>
-    </div>
+    <section className={s.error_page}>
+        <img src={require('../../../assets/errors/not-found/Frame 1 (1).svg').default} alt=""/>
+        <img src={require('../../../assets/errors/not-found/404 Error-pana 1.svg').default} alt=""/>
+        <p className={s.error_message}>Sorry, but we couldn’t found path <span className={s.path_name}>{location.pathname}</span></p>
+        <div className={s.buttons}>
+            <button onClick={() => goBack()}>Go Back</button>
+            <button onClick={() => goHome()}>Go Home</button>
+        </div>
+    </section>
   )
 }
